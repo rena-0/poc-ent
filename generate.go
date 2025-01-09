706187b2ee352d5c456c744b097899d8d5df8a02
Audrey Renggli <audrey.renggli@ruag.ch>
2025-01-09 10:13:39 +0100

    update

--- generate.go
+++ generate.go
@@ -1,1 +1,14 @@
-//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate ./schema
+package main
+
+import (
+	"log"
+
+	_ "github.com/ent/ent/cmd/entc" // Import ent codegen tool
+	"github.com/rena-0/poc-ent/ent"  // Your ent package
+)
+
+func main() {
+	if err := ent.Generate(); err != nil {
+		log.Fatalf("running ent generate: %v", err)
+	}
+}
